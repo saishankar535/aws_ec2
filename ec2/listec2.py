8e633fc40e40c0dbe0fd9392bfdee3ac0b5b082b
saishankar535 <kodthiwada@gmail.com>
2018-06-28 08:24:05 -0400

    just an update

--- ec2/listec2.py
+++ ec2/listec2.py
@@ -1,5 +1,24 @@
 import boto3
+import click
 session = boto3.Session()
 ec2 = session.resource('ec2')
 type(ec2)
-for i in ec2.instances.all():print(i)
+
+@click.command()
+@click.option ('--project', default=None,
+               help = "Only instances for project")
+def list_instances(project):
+    instances = []
+    if project:
+        filter = [{'Name':'tag:Project','Values':[project]}]
+        print(filter)
+        instances = ec2.instances.filter(Filters=filter)
+    else:
+        instances = ec2.instances.all()
+    for i in instances:
+        print(i)
+        print(', '.join((
+            i.id,i.instance_type)))
+    return
+if __name__ == '__main__':
+    list_instances()
